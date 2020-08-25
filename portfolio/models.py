29f865c987ef9cfba3e67c50090357c2569a3e43
agus <agu.pala@gmail.com>
2020-08-25 20:36:31 -0300

    Models

--- portfolio/models.py
+++ portfolio/models.py
@@ -28,7 +28,7 @@ class Project(models.Model):
         format='JPEG',
         options={'quality': 60},
     )
-    web_link = models.URLField(verbose_name="Web Link", blank=True, default="", max_lenght=200)
+    web_link = models.URLField(verbose_name="Web Link", blank=True, default="", max_length=200)
     github_link = models.URLField(verbose_name="Github Link", max_length=200)
     created = models.DateTimeField(verbose_name="Created", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Updated", auto_now=True)
